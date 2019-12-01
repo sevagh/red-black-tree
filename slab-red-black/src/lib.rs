b1a8a819b0d27b500dcd9d136ae3df833a87307f
Sevag Hanssian <sevag.hanssian@gmail.com>
2019-12-01 09:23:57 -0500

    Fix insert+insert_fixup. WIP deletes

--- slab-red-black/src/lib.rs
+++ slab-red-black/src/lib.rs
@@ -64,45 +64,157 @@ T: std::default::Default + std::cmp::PartialOrd + std::fmt::Debug + Copy {
     pub fn insert(&mut self, key: T) {
         let z = self.slab.insert(Node::new(key, self.nil_sentinel));
 
-        // empty insert - root is black
-        if self.root == self.nil_sentinel {
+        let mut y = self.nil_sentinel;
+        let mut x = self.root;
+
+        while x != self.nil_sentinel {
+            y = x;
+            let dir = if self.slab[z].key < self.slab[x].key { 0 } else { 1 };
+            x = self.slab[x].children[dir];
+        }
+
+        self.slab[z].parent = y;
+        if y == self.nil_sentinel {
             self.root = z;
-            self.slab[self.root].parent = self.nil_sentinel;
-            self.slab[z].red = false;
-            return;
+        } else {
+            let dir = if self.slab[z].key < self.slab[y].key { 0 } else { 1 };
+            self.slab[y].children[dir] = z;
         }
 
         self.slab[z].red = true;
-        // find the appropriate spot to put a red leaf
-        let mut curr = self.root;
-        loop {
-            let direction = if self.slab[curr].key < self.slab[z].key { 1 } else { 0 };
-            let next = self.slab[curr].children[direction];
-            if next == self.nil_sentinel {
-                self.slab[curr].children[direction] = z;
-                self.slab[z].parent = curr;
-                break;
-            }
-            curr = next;
-        }
 
-        self.red_black_fixup(z);
+        self.insert_fixup(z);
     }
 
-    fn red_black_fixup(&mut self, z_param: usize) {
+    fn tree_minimum(&mut self, mut x: usize) {
+        let mut l = self.slab[x].children[0];
+        while l != self.nil_sentinel {
+            x = l;
+            l = self.slab[x].children[0];
+        }
+        return x;
+    }
+
+    fn tree_successor(&mut self, mut x: usize) {
+        if self.slab[x].children[1] != self.nil_sentinel {
+            return self.tree_minimum(x);
+        }
+        let mut y = self.slab[x].parent;
+        while y != self.nil_sentinel && x == self.slab[y].children[1] {
+            x = y;
+            y = self.slab[y].parent;
+        }
+        return y;
+    }
+
+    pub fn delete(&mut self, key: T) -> Option<T> {
+        let mut z: usize;
+
+        // nothing to delete
+        if let Some(found_idx) = self.search_(key) {
+            z = found_idx;
+        } else {
+            return None;
+        }
+
+        let mut x: usize;
+        let mut y: usize;
+
+        if self.slab[z].children[0] == self.nil_sentinel || self.slab[z].children[1] == self.nil_sentinel {
+            y = z;
+        } else {
+            y = self.tree_successor(z);
+        }
+
+        let dir = if self.slab[y].children[0] != self.nil_sentinel { 0 } else { 1 };
+        x = self.slab[y].children[dir];
+
+        let yp = self.slab[y].parent;
+
+        self.slab[x].parent = yp;
+
+        if yp = self.nil_sentinel {
+            self.root = x;
+        } else {
+            let p = self.
+            let dir = if y == self.slab[yp].children[0] { 0 } else { 1 };
+            self.slab[yp].children[dir] = x;
+        }
+
+        if y != z {
+            self.slab[y].key = self.slab[z].key;
+        }
+        if !self.slab[y].red {
+            self.delete_fixup(x);
+        }
+        self.slab.remove(z);
+        return if y != self.nil_sentinel { Some(self.slab[y].key) } else { None };
+    }
+
+    fn insert_fixup(&mut self, mut z: usize) {
+        let mut p = self.slab[z].parent;
+        let mut pp: usize;
+
+        while self.slab[p].red {
+            p = self.slab[z].parent;
+            pp = self.slab[p].parent;
+
+            let dir = if self.slab[pp].children[0] == p { 1 } else { 0 };
+
+            let y = self.slab[pp].children[dir];
+
+            if self.slab[y].red {
+                self.slab[p].red = false;
+                self.slab[y].red = false;
+                self.slab[pp].red = true;
+                z = pp;
+
+                // recompute parent and grandparent after changing z
+                p = self.slab[z].parent;
+            } else { // y is black, or nil sentinel
+                if z == self.slab[p].children[dir] {
+                    z = p;
+
+                    self.rotate(z, dir^1);
+
+                    // recompute parent and grandparent after rotation
+                    p = self.slab[z].parent;
+                    pp = self.slab[p].parent;
+                }
+                self.slab[p].red = false;
+                self.slab[pp].red = true;
+                self.rotate(pp, dir);
+            }
+        }
+
         // blacken the root
         self.slab[self.root].red = false;
     }
 
-    pub fn search(&mut self, key: T) -> Option<T> {
+    fn delete_fixup(&mut self, mut x: usize) {
+        while x != self.root && !self.slab[x].red {
+        }
+
+        // blacken x
+        self.slab[x].red = false;
+    }
+
+    fn search_(&mut self, key: T) -> Option<usize> {
         let mut curr = self.root;
 
         while curr != self.nil_sentinel {
             if self.slab[curr].key == key {
-                return Some(key);
+                return curr;
             }
             let direction = if self.slab[curr].key < key { 1 } else { 0 };
             curr = self.slab[curr].children[direction];
+        }
+        return None;
+    }
+
+    pub fn search(&mut self, key: T) -> Option<T> {
+        if let Some(found_idx) = self.search_(key) {
+            return Some(self.slab[found_idx].key);
         }
         None
     }
@@ -129,19 +241,23 @@ T: std::default::Default + std::cmp::PartialOrd + std::fmt::Debug + Copy {
 
         while !queue.is_empty() {
             let curr = queue.pop_front().unwrap();
-            if curr == NULL {
+            if curr == self.nil_sentinel {
                 break;
             }
 
-            let left_child = self.slab[curr].children[0];
-            if left_child != NULL {
-                assert!(!self.slab[curr].red || (self.slab[curr].red && !self.slab[left_child].red)); // red node, non-red child
-                queue.push_back(left_child);
+            let l = self.slab[curr].children[0];
+            let r = self.slab[curr].children[1];
+
+            // red node must not have red children
+            if self.slab[curr].red {
+                assert!(!self.slab[l].red && !self.slab[r].red, "red node has red children");
             }
-            let right_child = self.slab[curr].children[1];
-            if right_child != NULL {
-                assert!(!self.slab[curr].red || (self.slab[curr].red && !self.slab[right_child].red)); // red node, non-red child
-                queue.push_back(left_child);
+
+            if l != self.nil_sentinel {
+                queue.push_back(l);
+            }
+            if r != self.nil_sentinel {
+                queue.push_back(r);
             }
         }
 
@@ -171,13 +287,8 @@ mod tests {
         let mut rb: RedBlack<i32> = RedBlack::new();
 
         rb.insert(5);
-        println!("insert 1 succeeded");
-
         rb.insert(6);
-        println!("insert 2 succeeded");
-
         rb.insert(7);
-        println!("insert 3 succeeded");
 
         assert_eq!(rb.search(5), Some(5));
         assert_eq!(rb.search(6), Some(6));
